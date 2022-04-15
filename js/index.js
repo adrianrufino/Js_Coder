8dafb2ce1fe859d13b8719202e3ed0791e689309
Adrián Rufino <98433626+adrianrufino@users.noreply.github.com>
2022-04-15 18:54:29 -0300

    ARMADO DE ESTRUCTURA
    Armado de estructura y cambios generales

---
+++ js/index.js
@@ -0,0 +1,12 @@
+for (let turnos= 1; turnos <=10; turnos++){
+    let ingreseNombre= prompt("Ingrese su Nombre y Apellido");
+    alert(" Ingreso el Nombre:" + " " + ingreseNombre + "." + " " + "Su turno es: "+ turnos);
+}
+    alert("El dia de hoy se agotaron los turnos. Por favor intente el dia de mañana");
+
+
+let salida = prompt("Para salir, por favor escriba ESC respetando las mayusculas")
+while (salida != "ESC"){
+    alert("Por favor ingresar un comando valido")
+    salida = prompt("Ingresar ESC para poder salir")
+}

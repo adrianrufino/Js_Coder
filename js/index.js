bb94ee1d1bb89f019b0b00ea20ca4f6dc1091251
adrianrufino <98433626+adrianrufino@users.noreply.github.com>
2022-04-21 15:12:04 -0300

    act
    act

--- js/index.js
+++ js/index.js
@@ -1,107 +1,30 @@
-/*
-// TURNERO
-// for (let turnos = 1; turnos <= 10; turnos++) {
-//     let ingreseNombre = prompt("Ingrese su Nombre y Apellido");
-//     alert(" Ingreso el Nombre:" + " " + ingreseNombre + "." + " " + "Su turno es: " + turnos);
-// }
-// alert("El dia de hoy se agotaron los turnos. Por favor intente el dia de mañana");
-
-
-// let salida = prompt("Para salir, por favor escriba ESC respetando las mayusculas")
-// while (salida != "ESC") {
-//     alert("Por favor ingresar un comando valido")
-//     salida = prompt("Ingresar ESC para poder salir")
-//
-
-
-
-
 // PROMEDIO NOTAS
 
-let nombreSolicitado = prompt ("ingrese su Nombre y Apellido");
 
-let nota1 = prompt("Ingrese la primera Nota");
-let nota2 = prompt("Ingrese la segunda Nota");
-let nota3 = prompt("Ingrese la tercera Nota");
-let nota4 = prompt("Ingrese la cuarta Nota");
-let nota5 = prompt("Ingrese la quinta Nota");
-let sumaNota = (parseInt(nota1) + parseInt(nota2) + parseInt(nota3) + parseInt(nota4) + parseInt(nota5));
-var promedioNota = (sumaNota/5);
+let nombreSolicitado = '';
+let nota1, nota2, nota3, nota4, nota5 = 0;
 
-function solicitarNombre () {
-    let nombreSolicitado = prompt ("Ingrese su Nombre y Apellido")
+solicitarDatos();
+
+promedio(nota1, nota2, nota3, nota4, nota5);
+
+function solicitarDatos() {
+    nombreSolicitado = prompt("Ingrese su Nombre y Apellido");
+    nota1 = prompt("Ingrese la primera Nota");
+    nota2 = prompt("Ingrese la segunda Nota");
+    nota3 = prompt("Ingrese la tercera Nota");
+    nota4 = prompt("Ingrese la cuarta Nota");
+    nota5 = prompt("Ingrese la quinta Nota");
 }
 
-alert("su promedio es"+""+ promedioNota);
+function promedio(n1, n2, n3, n4, n5) {
 
-//function solicitarDatos (nota1, nota2, nota3, nota4, nota5) {
- //   for (let notas; notas <= 5; notas++) 
-   // return (" Ingreso el Nombre:" + " " + nombreSolicitado + "." + " " + "Su turno promedio es: " promedioNota  );
-//    }
-//}
+    let promedioNota = 0.0;
 
-
-//solicitarNombre ();
-//olicitarDatos ();
-
-
-//parseInt(prompt("Ingrese la primera Nota"))
-
-// let nota1= Number;
-// let nota2= Number;
-// let nota3= Number;
-// let nota4= Number;
-// let nota5= Number;
-*/
-
-// var txt1, acumulador = 0;
-
-// for(var i = 0; i <=4; i++ ){
-//     txt1 =parseInt(prompt("ingrese nota :  "));
-//     acumulador = acumulador + txt1;
-//     }
-// alert = parseInt(" el promedio de notas es:  ",(acumulador/5).toFixed(2));
-
-
-
-
-
-
-
-
-function solicitarNombre () {
-    let nombreSolicitado = prompt ("Ingrese su Nombre y Apellido")
-}
-
-function promedio(){
-
-
-
-let nota1 = prompt("Ingrese la primera Nota");
-let nota2 = prompt("Ingrese la segunda Nota");
-let nota3 = prompt("Ingrese la tercera Nota");
-let nota4 = prompt("Ingrese la cuarta Nota");
-let nota5 = prompt("Ingrese la quinta Nota");
-
-
-for(i=0; i<=5; i++){
-let sumaNota = (parseFloat(nota1) + parseFloat(nota2) + parseFloat(nota3) + parseFloat(nota4) + parseFloat(nota5));
-let promedioNota = (sumaNota/5);
-alert(nombreSolicitado + "su promedio es"+" "+ promedioNota);
-
-}
-}
-
-function salir(){
-    while (salida != "ESC") {
-    alert("Por favor ingresar un comando valido")
-    salida = prompt("Ingresar ESC para poder salir")
-    
+    for (i = 0; i <= 4; i++) {
+        let sumaNota = (parseInt(n1) + parseInt(n2) + parseInt(n3) + parseInt(n4) + parseInt(n5));
+        promedioNota = (sumaNota / 5);
     }
 
+    alert(nombreSolicitado + ", su promedio es " + promedioNota +"." + " " + "Muchas gracias por su consulta");
 }
-
-solicitarNombre();
-promedio();
-salir();
-
